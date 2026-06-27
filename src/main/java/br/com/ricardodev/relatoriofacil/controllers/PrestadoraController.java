198977413aa77c4c52db58c535c8e1d1b3fdfe6f
Ric-ardo28 <ricardo2803br@hotmail.com>
2026-06-26 22:32:30 -0300

    adiciona métodos findById, insert, update e delete ao PrestadoraService e endpoints correspondentes no PrestadoraController

--- src/main/java/br/com/ricardodev/relatoriofacil/controllers/PrestadoraController.java
+++ src/main/java/br/com/ricardodev/relatoriofacil/controllers/PrestadoraController.java
@@ -6,9 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,10 +19,35 @@ public class PrestadoraController {
     private final PrestadoraService prestadoraService;
 
     @GetMapping
-    public ResponseEntity<Page<PrestadoraDTO>> findAll(Pageable pageable){
+    public ResponseEntity<Page<PrestadoraDTO>> findAll(Pageable pageable) {
         Page<PrestadoraDTO> prestadoraDTO = prestadoraService.findAll(pageable);
         return ResponseEntity.ok(prestadoraDTO);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<PrestadoraDTO> findById(@PathVariable Long id) {
+        PrestadoraDTO dto = prestadoraService.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<PrestadoraDTO> insert(@RequestBody PrestadoraDTO prestadoraDTO) {
+        prestadoraDTO = prestadoraService.insert(prestadoraDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(prestadoraDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(prestadoraDTO);
+
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<PrestadoraDTO> update(@PathVariable Long id, @RequestBody PrestadoraDTO prestadoraDTO){
+        prestadoraDTO = prestadoraService.update(id, prestadoraDTO);
+        return ResponseEntity.ok(prestadoraDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        prestadoraService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/main/java/br/com/ricardodev/relatoriofacil/services/PrestadoraService.java
+++ src/main/java/br/com/ricardodev/relatoriofacil/services/PrestadoraService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @RequiredArgsConstructor
 @Service
 public class PrestadoraService {
@@ -20,6 +21,44 @@ public class PrestadoraService {
     public Page<PrestadoraDTO> findAll(Pageable pageable) {
         Page<Prestadora> prestadoras = prestadoraRepository.findAll(pageable);
         return prestadoras.map(PrestadoraDTO::new);
+    }
+
+    @Transactional(readOnly = true)
+    public PrestadoraDTO findById(Long id) {
+        Prestadora prestadora = prestadoraRepository.findById(id).get();
+        return new PrestadoraDTO(prestadora);
+    }
+
+    @Transactional
+    public PrestadoraDTO insert(PrestadoraDTO prestadoraDTO) {
+        Prestadora entity = new Prestadora();
+        copiarDadosDTO(prestadoraDTO, entity);
+        entity = prestadoraRepository.save(entity);
+        return new PrestadoraDTO(entity);
+    }
+
+    public PrestadoraDTO update(Long id, PrestadoraDTO prestadoraDTO) {
+        Prestadora entity = prestadoraRepository.getReferenceById(id);
+        copiarDadosDTO(prestadoraDTO, entity);
+        entity = prestadoraRepository.save(entity);
+        return new PrestadoraDTO(entity);
+    }
+
+    public void delete(Long id) {
+        prestadoraRepository.deleteById(id);
+    }
+
+
+    private void copiarDadosDTO(PrestadoraDTO prestadoraDTO, Prestadora entity) {
+        entity.setId(prestadoraDTO.getId());
+        entity.setRazaoSocial(prestadoraDTO.getRazaoSocial());
+        entity.setNomeFantasia(prestadoraDTO.getNomeFantasia());
+        entity.setCnpj(prestadoraDTO.getCnpj());
+        entity.setInscricaoEstadual(prestadoraDTO.getInscricaoEstadual());
+        entity.setEndereco(prestadoraDTO.getEndereco());
+        entity.setTelefone(prestadoraDTO.getTelefone());
+        entity.setEmail(prestadoraDTO.getEmail());
+        entity.setAtivo(prestadoraDTO.getAtivo());
     }
 
 
